ffab0bf3b498a2371923f0c6d5725d7472d936e1
Bbanez <banezprod@gmail.com>
2022-02-28 15:15:17 +0100

    Add get all

--- src/handlers/template.ts
+++ src/handlers/template.ts
@@ -12,6 +12,13 @@ export function createBcmsClientTemplateHandler({
   const basePath = '/template';
 
   return {
+    async getAll() {
+      const result = await send<{ items: BCMSTemplate[] }>({
+        url: `${basePath}/all`,
+        method: 'GET',
+      });
+      return result.items;
+    },
     async get(data) {
       const result = await send<{ item: BCMSTemplate }>({
         url: `${basePath}/${data.template}`,
--- src/types/handlers/template.ts
+++ src/types/handlers/template.ts
@@ -2,4 +2,5 @@ import type { BCMSTemplate } from '../models';
 
 export interface BCMSClientTemplateHandler {
   get(data: { template: string }): Promise<BCMSTemplate>;
+  getAll(): Promise<BCMSTemplate[]>;
 }
